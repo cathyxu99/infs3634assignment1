bef971a998db3af39927b48ec26128a3c6207e18
Aditya <adityavn08@gmail.com>
2020-04-24 01:19:31 +1000

    Notes: Added basic new Note fragment

---
+++ app/src/main/java/com/example/infs3634assignment/Notes/NewNoteFragment.java
@@ -0,0 +1,34 @@
+package com.example.infs3634assignment.Notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.infs3634assignment.R;
+
+
+public class NewNoteFragment extends Fragment {
+
+    public NewNoteFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_new_note, container, false);
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/infs3634assignment/Notes/NotesFragment.java
+++ app/src/main/java/com/example/infs3634assignment/Notes/NotesFragment.java
@@ -4,14 +4,19 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.infs3634assignment.R;
+import com.example.infs3634assignment.TheBodyFragment;
+import com.example.infs3634assignment.blankHomeActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,16 +48,31 @@ public class NotesFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_notes, container, false);
 
         noteList = rootView.findViewById(R.id.noteList);
-
         System.out.println("Length of notes: " + notes.size());
 
         nAdapter = new NoteAdapter(rootView.getContext(), notes);
-
         System.out.println("Length of adapter: " + nAdapter.getItemCount());
 
         noteList.setAdapter(nAdapter);
-
         noteList.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+
+        //Set button
+        final blankHomeActivity homeActivity = (blankHomeActivity) getActivity();
+
+
+        ImageButton addNewNote = rootView.findViewById(R.id.addNote);
+        addNewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = homeActivity.getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragment = new NewNoteFragment();
+                Bundle arguments = new Bundle();
+                fragment.setArguments(arguments);
+                transaction.replace(R.id.mainFragContainer,fragment);
+                transaction.commit();
+            }
+        });
 
         return rootView;
     }
